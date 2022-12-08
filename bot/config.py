252ef23258d2606d30a2515edc8d215afe568b97
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2022-12-08 20:16:19 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,13 +4,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID","6534707", cast=int)
+    APP_ID = config("APP_ID", "6534707",cast=int)
     API_HASH = config("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
-    BOT_TOKEN = config("BOT_TOKEN","5442493323:AAGxnHg1RVxo8XyDdtfDbHTQrcid2mGqNVM")
+    BOT_TOKEN = config("BOT_TOKEN","5257391930:AAFYsASH5gVmOfPiUOl66Mk5CYbexANXSBU")
     DEV = 1664850827
     OWNER = config("OWNER","1430593323")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
-    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/de709efff463ace84b278.jpg")
+    THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/f9e5d783542906418412d.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
